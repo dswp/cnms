31208d84b8d2ff5308d9425c186bc7631e09af51
Refat <nobilix@gmail.com>
2017-05-11 15:49:16 +0300

    add the subtitles support, small refactoring of metaboxes

--- wp-content/themes/twentyseventeen-child/framework/movies-metabox.php
+++ wp-content/themes/twentyseventeen-child/framework/movies-metabox.php
@@ -6,7 +6,12 @@ function movie_product_options_get_meta( $value ) {
 	if ( ! empty( $field ) ) {
 		return is_array( $field ) ? stripslashes_deep( $field ) : stripslashes( wp_kses_decode_entities( $field ) );
 	} else {
-		return 1;
+		switch( $value ) {
+			case 'movie_product_options_price':
+				return 1; // 1 dollar default movie price
+			default:
+				return '';
+		}
 	}
 }
 
@@ -24,11 +29,15 @@ add_action( 'add_meta_boxes', 'movie_product_options_add_meta_box' );
 
 function movie_product_options_html( $post) {
 	wp_nonce_field( '_movie_product_options_nonce', 'movie_product_options_nonce' ); ?>
-
+	<p>
+		<label for="movie_product_options_subtitle"><?php _e( 'Subtitle', 'cnms' ); ?></label><br>
+		<input type="text" name="movie_product_options_subtitle" id="movie_product_options_subtitle" value="<?php echo movie_product_options_get_meta( 'movie_product_options_subtitle' ); ?>" style="width: 100%;">
+	</p>
 	<p>
 		<label for="movie_product_options_price"><?php _e( 'Price', 'cnms' ); ?></label><br>
 		<input type="text" name="movie_product_options_price" id="movie_product_options_price" value="<?php echo movie_product_options_get_meta( 'movie_product_options_price' ); ?>">
-	</p><?php
+	</p>
+	<?php
 }
 
 function movie_product_options_save( $post_id ) {
@@ -36,12 +45,14 @@ function movie_product_options_save( $post_id ) {
 	if ( ! isset( $_POST['movie_product_options_nonce'] ) || ! wp_verify_nonce( $_POST['movie_product_options_nonce'], '_movie_product_options_nonce' ) ) return;
 	if ( ! current_user_can( 'edit_post', $post_id ) ) return;
 
+	if ( isset( $_POST['movie_product_options_subtitle'] ) )
+		update_post_meta( $post_id, 'movie_product_options_subtitle', esc_attr( $_POST['movie_product_options_subtitle'] ) );
+
 	if ( isset( $_POST['movie_product_options_price'] ) )
-		update_post_meta( $post_id, 'movie_product_options_price', esc_attr( $_POST['movie_product_options_price'] ) );
+		update_post_meta( $post_id, 'movie_product_options_price',  floatval( esc_attr( $_POST['movie_product_options_price'] ) ) );
 }
 add_action( 'save_post', 'movie_product_options_save' );
 
-movie_product_options_get_meta( 'movie_product_options_price' );
 
 /*
 * Return woocommerce price value from CPT custom field
