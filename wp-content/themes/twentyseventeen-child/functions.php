78417b5ff4bc58dc83f2c7f23d54c56ed2ce6330
Refat <nobilix@gmail.com>
2017-05-11 14:45:33 +0300

    Show movies archive as a front page (for demo purpose)

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -18,3 +18,19 @@ require_once( get_stylesheet_directory() . '/framework/movie-post-type.php' );
 require_once( get_stylesheet_directory() . '/framework/movies-metabox.php' );
 require_once( get_stylesheet_directory() . '/framework/movie-cpt-woocommerce-support.php' );
 require_once( get_stylesheet_directory() . '/framework/custom-registration.php' );
+
+
+// Show movies archive as a front page (for demo purpose)
+add_action("pre_get_posts", "custom_front_page");
+function custom_front_page($wp_query) {
+	if ( is_admin() ) return;
+
+	if ( $wp_query->get('page_id') == get_option('page_on_front') ) {
+		$wp_query->set( 'post_type', 'movie' );
+		$wp_query->set( 'page_id', '' );
+		$wp_query->is_page = 0;
+		$wp_query->is_singular = 0;
+		$wp_query->is_post_type_archive = 1;
+		$wp_query->is_archive = 1;
+	}
+}
